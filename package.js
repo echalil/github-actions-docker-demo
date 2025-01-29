5cdcb0708f705364980e3e7f7733da7861322cb5
Echo <132055637+echalil@users.noreply.github.com>
2025-01-29 17:34:30 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "node-docker-demo",
+  "version": "1.0.0",
+  "description": "A simple Node.js app with GitHub Actions and Docker",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
